13305284839d1c99f4b95228806bbe7f26a73633
wq9578 <121522862+wq9578@users.noreply.github.com>
2023-10-14 19:45:21 +0200

    Installation of Filament v3

--- install.zsh
+++ install.zsh
@@ -6,3 +6,8 @@
 
 laravel new jetstream-filament-app -n --jet --api --teams --stack=livewire
 cd jetstream-filament-app
+
+# https://filamentphp.com/docs/3.x/panels/installation
+# with '-n' for non-interactive installation
+composer require filament/filament:"^3.0-stable" -W
+php artisan -n filament:install --panels
