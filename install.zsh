7d680b3d081bae134b46cbcfe2d8016ffae7efd0
wq9578 <121522862+wq9578@users.noreply.github.com>
2023-10-14 22:52:50 +0200

    Comments updated

--- install.zsh
+++ install.zsh
@@ -5,9 +5,11 @@
 # Then also install npm linked at https://laravel.com/docs/10.x/installation#your-first-laravel-project
 # And MySQL as described at https://formulae.brew.sh/formula/mysql
 
+# Install Laravel with Jetstream (Livewire, Teams, API)
 laravel new jetstream_filament_app_test -n --jet --api --teams --stack=livewire
 cd jetstream_filament_app_test
 
+# Install Filament v3
 # https://filamentphp.com/docs/3.x/panels/installation
 # with '-n' for non-interactive installation
 composer require filament/filament:"^3.0-stable" -W
@@ -16,5 +18,6 @@ php artisan -n filament:install --panels
 # Drop and create database
 echo "drop database if exists jetstream_filament_app_test; create database jetstream_filament_app_test;" | mysql -u root
 
+# Create database tables
 # https://laravel.com/docs/10.x/installation#databases-and-migrations
-php artisan migrate # create database
+php artisan migrate
