fbae042bab2f96ec7fd11f1208f6fc5b6670c360
wq9578 <121522862+wq9578@users.noreply.github.com>
2023-10-14 20:58:44 +0200

    Automatically create fresh database (and drop any existing one)

--- install.zsh
+++ install.zsh
@@ -1,13 +1,20 @@
 #!/bin/zsh -e
 
-# Installation on macOS
+# Installation on macOS with MySQL:
 # First download and install Laravel Herd for macOS from https://herd.laravel.com
 # Then also install npm linked at https://laravel.com/docs/10.x/installation#your-first-laravel-project
+# And MySQL as described at https://formulae.brew.sh/formula/mysql
 
-laravel new jetstream-filament-app -n --jet --api --teams --stack=livewire
-cd jetstream-filament-app
+laravel new jetstream_filament_app_test -n --jet --api --teams --stack=livewire
+cd jetstream_filament_app_test
 
 # https://filamentphp.com/docs/3.x/panels/installation
 # with '-n' for non-interactive installation
 composer require filament/filament:"^3.0-stable" -W
 php artisan -n filament:install --panels
+
+# Drop and create database
+echo "drop database if exists jetstream_filament_app_test; create database jetstream_filament_app_test;" | mysql -u root
+
+# https://laravel.com/docs/10.x/installation#databases-and-migrations
+php artisan migrate # create database
